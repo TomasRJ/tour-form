4dac8990a739649f69991cd274b8e02339b4bd55
Tomas R J <49104467+TomasRJ@users.noreply.github.com>
2023-09-10 13:16:41 +0200

    Tilføjede rabbitmq funktionalitet i webform

--- webform/Pages/Index.cshtml.cs
+++ webform/Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using RabbitMQ.Client;
+using System.Text;
 using webform.Models;
 
 namespace webform.Pages;
@@ -27,35 +29,49 @@ public class IndexModel : PageModel
     public IActionResult OnPost()
     {
         if (!ModelState.IsValid || FormData == null || FormData.SelectedCountryId == 0)
-            {
-                return Page();
-            }    
+        {
+            return Page();
+        }
 
         var countries = GetCountriesList();
-        
-        var book = FormData.Book ? "ja" : "nej";
+
+        var book = FormData.Book ? "Booked" : "Cancelled";
         var country = countries.FirstOrDefault(x => x.Value == FormData.SelectedCountryId.ToString())!.Text;
-        
+
         // _logger.LogInformation($"FormData values: {FormData.Name}, {FormData.Email}, {FormData.SelectedCountryId}, {FormData.Book}, Output: {book}, {country}");
-        
+
+        SendRabbitMQMessage(book, country, FormData.Name!, FormData.Email!);
         // Redirect or return a view
         return RedirectToPage("Index"); // You can redirect to the same page or another page.
     }
-    private List<SelectListItem> GetCountriesList() 
-{
-    var selectList = new List<SelectListItem>
+
+    private static void SendRabbitMQMessage(string? book, string country, string name, string email)
     {
-        new SelectListItem{ Value = "1", Text = "Danmark"},
-        new SelectListItem{ Value = "2", Text = "Sverige"},
-        new SelectListItem{ Value = "3", Text = "Norge"},
-        new SelectListItem{ Value = "4", Text = "Finland"},
-        new SelectListItem{ Value = "5", Text = "Island"},
-        new SelectListItem{ Value = "6", Text = "Færøerne"},
-        new SelectListItem{ Value = "7", Text = "Grønland"},
-        new SelectListItem{ Value = "8", Text = "Tyskland"},
-        new SelectListItem{ Value = "9", Text = "England"},
-        new SelectListItem{ Value = "10", Text = "Frankrig"}
-    };
-    return selectList.OrderBy(x => x.Text).ToList();
-}
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        channel.ExchangeDeclare(exchange: "Tour", type: ExchangeType.Topic);
+
+        var routingKey = book != null ? "Tour." + book : "anonymous.info";
+        var message = book != null ? $"A tour to {country} from the user \"{name} ({email})\" has been {book}" : "No tour booked";
+        var body = Encoding.UTF8.GetBytes(message);
+        channel.BasicPublish(exchange: "Tour", routingKey: routingKey, basicProperties: null, body: body);
+    }
+    private List<SelectListItem> GetCountriesList() 
+    {
+        var selectList = new List<SelectListItem>
+        {
+            new SelectListItem{ Value = "1", Text = "Danmark"},
+            new SelectListItem{ Value = "2", Text = "Sverige"},
+            new SelectListItem{ Value = "3", Text = "Norge"},
+            new SelectListItem{ Value = "4", Text = "Finland"},
+            new SelectListItem{ Value = "5", Text = "Island"},
+            new SelectListItem{ Value = "6", Text = "Færøerne"},
+            new SelectListItem{ Value = "7", Text = "Grønland"},
+            new SelectListItem{ Value = "8", Text = "Tyskland"},
+            new SelectListItem{ Value = "9", Text = "England"},
+            new SelectListItem{ Value = "10", Text = "Frankrig"}
+        };
+        return selectList.OrderBy(x => x.Text).ToList();
+    }
 }

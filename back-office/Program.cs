2cde7c17f3e03b1b47bc4e952c6101803b4a3ffc
Tomas R J <49104467+TomasRJ@users.noreply.github.com>
2023-09-10 13:22:06 +0200

    To apps der bruger rabbitmq topic exchange

---
+++ back-office/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "Tour", type: ExchangeType.Topic);
+// declare a server-named queue
+var queueName = channel.QueueDeclare().QueueName;
+channel.QueueBind(queue: queueName, exchange: "Tour", routingKey: "Tour.*");
+
+var textInput = " ";
+while(textInput != "")
+{ 
+    Console.WriteLine(" [*] Waiting for messages. To exit just press enter");
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine($"{message}");
+    };
+    channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+    textInput = Console.ReadLine();
+}
